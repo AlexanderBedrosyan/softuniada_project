b5243d22d6fa05ff8923788b9bfdf95be75177cb
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-02-25 00:50:19 +0200

    Fix the User view.

--- frontend/app/components/forms/registrationForm.js
+++ frontend/app/components/forms/registrationForm.js
@@ -15,7 +15,7 @@ const RegistrationForm = () => {
       const registrationData = { username, email, password };
 
       // Make a POST request to your API endpoint
-      const response = await fetch("/api/register", {
+      const response = await fetch("http://127.0.0.1:8000/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
