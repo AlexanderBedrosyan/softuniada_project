0bd48dcc058c7ab2a8b7a05704ca8818ed490475
KaloyanBehov <135109858+KaloyanBehov@users.noreply.github.com>
2024-02-25 16:20:25 +0200

    Update page.js
    .

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,11 @@
 "use client";
 import RegistrationForm from "./components/forms/registrationForm";
 import { NextUIProvider } from "@nextui-org/system";
-
+// import {useSession}
+// const {data, status} = useSession()
+// if (status == authenticated) {
+// redirect to homepage  
+} 
 export default function Home() {
   return (
     <main className="">
