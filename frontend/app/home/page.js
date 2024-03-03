01f414f67c9be04b382915a85b61515dec649d7c
KaloyanBehov <kaloqnbehov@gmail.com>
2024-03-03 06:48:06 +0300

    hydration fixed

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -2,18 +2,24 @@
 import { useContext } from "react";
 import AuthContext from "@/contexts/authContext";
 import AuthPage from "../auth/page";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
   const { user } = useContext(AuthContext);
+  const [isClient, setIsclient] = useState(false);
+  useEffect(() => {
+    setIsclient(true);
+  });
+
   return (
     <div>
-      {user ? (
-        <div>
-          <p>{`${user?.email}`}</p>
-        </div>
-      ) : (
-        <AuthPage />
-      )}
+      {user
+        ? isClient && (
+            <div>
+              <p>{`${user?.email}`}</p>{" "}
+            </div>
+          )
+        : isClient && <AuthPage />}
     </div>
   );
 };
