1336005eee623863643a8406513b8e8e4d7f7e15
Yanko Rashkov <thewhitedead@abv.bg>
2024-03-25 14:10:51 +0200

    test

--- testing_script.py
+++ testing_script.py
@@ -1,30 +1,30 @@
-import json
-import os
-from datetime import timedelta, date
-
-import django
-from django.contrib.auth.hashers import check_password
-from django.db.models import Avg, Sum
-
-# Set up Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "softuniada_project.settings")
-django.setup()
-
-# Import your models here
-
-
-from backend.models import User, Rating
-
-# Create queries within functions
-
-# Avarage rating
-User.objects.create(username='marin', email='marin@marin.com', password='Blas@2352354')
-
-user = User.objects.get(username='admin')
-print(user)
-rating_instance = Rating.objects.create(rating=3)
-rating_instance.user.add(user)
-
-user_rating = Rating.objects.filter(user=user).first()
-average_rating = user_rating.average_rating()
-print(f"{Rating.objects.filter(user=user).first()}")
+# import json
+# import os
+# from datetime import timedelta, date
+#
+# import django
+# from django.contrib.auth.hashers import check_password
+# from django.db.models import Avg, Sum
+#
+# # Set up Django
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "softuniada_project.settings")
+# django.setup()
+#
+# # Import your models here
+#
+#
+# from backend.models import User, Rating
+#
+# # Create queries within functions
+#
+# # Avarage rating
+# User.objects.create(username='marin', email='marin@marin.com', password='Blas@2352354')
+#
+# user = User.objects.get(username='admin')
+# print(user)
+# rating_instance = Rating.objects.create(rating=3)
+# rating_instance.user.add(user)
+#
+# user_rating = Rating.objects.filter(user=user).first()
+# average_rating = user_rating.average_rating()
+# print(f"{Rating.objects.filter(user=user).first()}")
