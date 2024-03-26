11ded4f9db9d58ef03f52ed453f24309d106289d
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-03-26 12:19:19 +0200

    Update testing_script

--- testing_script.py
+++ testing_script.py
@@ -1,41 +1,41 @@
-import json
-import os
-from datetime import timedelta, date
-
-import django
-from django.contrib.auth.hashers import check_password
-from django.db.models import Avg, Sum
-
-# Set up Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "softuniada_project.settings")
-django.setup()
-
-# Import your models here
-
-
-from backend.models import User, Rating, Voter
-
-# Create queries within functions
-
+# import json
+# import os
+# from datetime import timedelta, date
+#
+# import django
+# from django.contrib.auth.hashers import check_password
+# from django.db.models import Avg, Sum
+#
+# # Set up Django
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "softuniada_project.settings")
+# django.setup()
+#
+# # Import your models here
+#
+#
+# from backend.models import User, Rating, Voter
+#
+# # Create queries within functions
+#
 # Avarage rating
 # Rating.objects.all().delete()
-
-user = User.objects.get(username='Kaloqn')
-user1 = User.objects.get(username='Alexander')
-user2 = User.objects.get(username='Dragomir')
-
-
-rating_instance = Rating.objects.create(rating=3)
-rating_instance2 = Rating.objects.create(rating=4)
-
-Voter.objects.create(voter=user1, voted_user=rating_instance, id_voted_user=user, rating=rating_instance.rating)
-Voter.objects.create(voter=user2, voted_user=rating_instance2, id_voted_user=user, rating=rating_instance2.rating)
-
-
+#
+# user = User.objects.get(username='Kaloqn')
+# user1 = User.objects.get(username='Alexander')
+# user2 = User.objects.get(username='Dragomir')
+#
+#
+# rating_instance = Rating.objects.create(rating=3)
+# rating_instance2 = Rating.objects.create(rating=4)
+#
+# Voter.objects.create(voter=user1, voted_user=rating_instance, id_voted_user=user, rating=rating_instance.rating)
+# Voter.objects.create(voter=user2, voted_user=rating_instance2, id_voted_user=user, rating=rating_instance2.rating)
+#
+#
 #
 # all_ratings = Rating.objects.filter(user=user)
 # print(all_ratings)
-
+#
 # rating_instance = Rating.objects.create(rating=5)
 # rating_instance.user.add(user1)
 #
